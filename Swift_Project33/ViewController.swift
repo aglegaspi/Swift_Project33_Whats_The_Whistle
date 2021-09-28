6dba32c3a5425237bc9a090ffd73a4870783ef17
aglegaspi <lex83nyc@gmail.com>
2021-09-28 14:03:45 -0700

    add navigation items and objc function

---
+++ Swift_Project33/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift_Project33
+//
+//  Created by Alex 6.1 on 9/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "What's that Whistle?"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWhistle))
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
+    }
+    
+    @objc func addWhistle() {
+        //let vc = RecordWhistleViewController()
+        //navigationController?.pushViewController(vc, animated: true)
+    }
+
+
+}
+
