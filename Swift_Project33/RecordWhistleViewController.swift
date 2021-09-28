df4505013650daa8ed52c139b2ce1c33b418c47e
aglegaspi <lex83nyc@gmail.com>
2021-09-28 14:06:07 -0700

    create RecordWhistleViewController

---
+++ Swift_Project33/RecordWhistleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecordWhistleViewController.swift
+//  Swift_Project33
+//
+//  Created by Alex 6.1 on 9/28/21.
+//
+
+import UIKit
+
+class RecordWhistleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
