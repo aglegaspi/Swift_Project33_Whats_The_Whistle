189e2bbf9d37d295f6cb41b931d9d5836660764b
aglegaspi <lex83nyc@gmail.com>
2021-09-28 14:15:30 -0700

    set up Stack View

--- Swift_Project33/RecordWhistleViewController.swift
+++ Swift_Project33/RecordWhistleViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class RecordWhistleViewController: UIViewController {
+    
+    var stackView: UIStackView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +18,23 @@ class RecordWhistleViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func loadView() {
+        view = UIView()
+        
+        view.backgroundColor = .gray
+        
+        stackView = UIStackView()
+        stackView.spacing = 30
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackView.Distribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 
     /*
     // MARK: - Navigation
