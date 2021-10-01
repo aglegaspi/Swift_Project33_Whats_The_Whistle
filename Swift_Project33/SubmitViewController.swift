3408348bc8f8444dba9db7d62b8a16f2bc609605
aglegaspi <lex83nyc@gmail.com>
2021-10-01 10:37:11 -0700

    create SubmitViewController

---
+++ Swift_Project33/SubmitViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SubmitViewController.swift
+//  Swift_Project33
+//
+//  Created by Alex 6.1 on 10/1/21.
+//
+
+import UIKit
+
+class SubmitViewController: UIViewController {
+    
+    var genre: String!
+    var comments: String!
+    
+    var stackVie: UIStackView!
+    var status: UILabel!
+    var spinner: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
