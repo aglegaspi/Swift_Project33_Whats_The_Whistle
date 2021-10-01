8b80bf78c9fdb19fddc486e49de21c6f3c4af012
aglegaspi <lex83nyc@gmail.com>
2021-10-01 10:37:37 -0700

    create stackview, spinner, and status

--- Swift_Project33/SubmitViewController.swift
+++ Swift_Project33/SubmitViewController.swift
@@ -12,13 +12,46 @@ class SubmitViewController: UIViewController {
     var genre: String!
     var comments: String!
     
-    var stackVie: UIStackView!
+    var stackView: UIStackView!
     var status: UILabel!
     var spinner: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .gray
+        
+        stackView =  UIStackView()
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackView.Distribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        status = UILabel()
+        status.translatesAutoresizingMaskIntoConstraints = false
+        status.text = "Submitting..."
+        status.textColor = .white
+        status.font = .preferredFont(forTextStyle: .title1)
+        status.numberOfLines = 0
+        status.textAlignment = .center
+        
+        spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        
+        stackView.addArrangedSubview(status)
+        stackView.addArrangedSubview(spinner)
     }
 
 }
