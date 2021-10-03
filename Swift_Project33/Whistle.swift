aab7b3b3f296d1e067e69f04678ce25f1f353d78
aglegaspi <lex83nyc@gmail.com>
2021-10-03 10:14:35 -0700

    create Whistle class

---
+++ Swift_Project33/Whistle.swift
@@ -0,0 +1,16 @@
+//
+//  Whistle.swift
+//  Swift_Project33
+//
+//  Created by Alex 6.1 on 10/3/21.
+//
+
+import UIKit
+import CloudKit
+
+class Whistle: NSObject {
+    var recordID: CKRecord.ID!
+    var genre: String!
+    var commentss: String!
+    var audio: URL!
+}
