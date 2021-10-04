850253b683479beccc35204d7ed5e4559baec25b
aglegaspi <lex83nyc@gmail.com>
2021-10-04 16:01:06 -0700

    select and deselect genres and add or remove from array

--- Swift_Project33/MyGenresViewController.swift
+++ Swift_Project33/MyGenresViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 class MyGenresViewController: UITableViewController {
     
     var myGenres: [String]!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let defaults = UserDefaults.standard
         if let savedGenres = defaults.object(forKey: "myGenres") as? [String] {
             myGenres = savedGenres
@@ -30,28 +30,52 @@ class MyGenresViewController: UITableViewController {
     @objc func saveTapped() {
         
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return SelectGenreViewController.genres.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let genre = SelectGenreViewController.genres[indexPath.row]
+        cell.textLabel?.text = genre
+        
+        if myGenres.contains(genre) {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
-    */
-
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            
+            let selectedGenre = SelectGenreViewController.genres[indexPath.row]
+            
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+                myGenres.append(selectedGenre)
+            } else {
+                cell.accessoryType = .none
+                
+                if let index = myGenres.firstIndex(of: selectedGenre) {
+                    myGenres.remove(at: index)
+                }
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    
 }
