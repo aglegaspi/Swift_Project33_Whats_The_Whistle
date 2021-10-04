5112c313fcdb520879645556bae23ae201d8b0cb
aglegaspi <lex83nyc@gmail.com>
2021-10-03 23:02:13 -0700

    set up properties and tableview to load sections and rows and datasource

--- Swift_Project33/ResultsViewController.swift
+++ Swift_Project33/ResultsViewController.swift
@@ -10,35 +10,87 @@ import AVFoundation
 import CloudKit
 
 class ResultsViewController: UITableViewController {
-
+    
+    //MARK: - PROPERTIES
+    var whistle: Whistle!
+    var suggestions = [String]()
+    var whistlePlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
-
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 1 {
+            return "Suggest Songs"
+        }
+        return nil
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+            return 1
+        } else {
+            return suggestions.count + 1
+        }
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.selectionStyle = .none
+        cell.textLabel?.numberOfLines = 0
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+            
+            if whistle.comments.count == 0 {
+                cell.textLabel?.text = "Comments: None"
+            } else {
+                cell.textLabel?.text = whistle.comments
+            }
+        } else {
+            cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            
+            if indexPath.row == suggestions.count {
+                cell.textLabel?.text = "Add suggestion"
+                cell.selectionStyle = .gray
+            } else {
+                cell.textLabel?.text = suggestions[indexPath.row]
+            }
+        }
+        
+        
         return cell
     }
-    */
-
-
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.section == 1 && indexPath.row == suggestions.count else { return }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let ac = UIAlertController(title: "Suggest A Song", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] action in
+            if let textField = ac.textFields?[0] {
+                if textField.text!.count > 0 {
+                    //self.add(suggestion: textField.text!)
+                }
+            }
+            
+        })
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+        
+    }
 }
